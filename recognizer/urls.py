6ed34588a2a55e9d68b7d932dbb6c4274c588062
parthishere <parthishere1234@gmail.com>
2022-03-25 08:28:49 +0530

    chnages

--- recognizer/urls.py
+++ recognizer/urls.py
@@ -13,7 +13,8 @@ from .views import (
     # login_with_face_part2, # login with recognizer Class
     # login_with_face_part3, # login with part 3 which consists of all the nessecity things inside on function!
     # test_frame
-    export_users_xls
+    export_users_xls,
+    change_whole_site_by_clicking
 )
 
 app_name = 'recognizer'
@@ -27,6 +28,7 @@ urlpatterns = [
     path('profile/<int:pk>', profile_view, name='profile'),
     path('profile/<int:pk>/update', update_profile_view, name='update-profile'),
     path('login-with-face', login_with_face, name='login-with-face'),
+    path('change-website', change_whole_site_by_clicking, name='change-website'),
     # path('login-with-face-2', login_with_face_part2, name='login-with-face-2'),
     # path('frame-check', test_frame, name='test-frame')
     
