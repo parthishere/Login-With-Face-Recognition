e6aa20b98e0b0e66486f3a47a49c312aa7c68add
parthishere <parthishere1234@gmail.com>
2022-05-07 15:18:53 +0530

    changes

--- login_with_face/settings.py
+++ login_with_face/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '3p__4+!#)2p30xb&+p2awyyw3rw1cl__akyns-$5sdomb!d+#j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['loginwface.herokuapp.com', '127.0.0.1', '192.168.0.102', '192.168.0.101', '192.168.0.100', '0.0.0.0', '*']
+ALLOWED_HOSTS = ['loginwface.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # 'channels',
+
     'recognizer',
     'login_details',
     'teacher',
