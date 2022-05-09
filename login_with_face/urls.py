42805f9348d406408c571fd5a6550251af70d912
parthishere <parthishere1234@gmail.com>
2022-05-09 18:00:22 +0530

    changes

--- login_with_face/urls.py
+++ login_with_face/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recognizer.urls', namespace='recognizer')),
-    path('teacher/', include('teacher.urls', namespace='teacher'))
+    path('teacher/', include('teacher.urls', namespace='teacher')),
+    re_path,(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT})
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
