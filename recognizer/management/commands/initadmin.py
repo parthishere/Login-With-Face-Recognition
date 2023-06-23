52a72e5a8d6ebdb4f72764563d07feb7000d4e70
“parthishere” <parthishere1234@gmail.com>
2023-06-23 10:25:02 +0530

    changes

--- recognizer/management/commands/initadmin.py
+++ recognizer/management/commands/initadmin.py
@@ -3,6 +3,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
 import dotenv
+from recognizer.models import UserProfile, LectrueModel
+from teacher.models import DistrictCollege, CityCollegeModel, CollegeModel, CollegeBranchModel
 
 dotenv_file = os.path.join(settings.BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
@@ -15,12 +17,40 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if User.objects.filter(is_superuser=True).count() == 0:
-            for user in settings.ADMINS:
-                username = os.environ['ADMIN_NAME']
-                email = os.environ['ADMIN_EMAIL']
-                password = os.environ['ADMIN_PASSWORD']
-                print('Creating account for %s (%s)' % (username, email))
-                admin = User.objects.create_superuser(
-                    email=email, username=username, password=password, is_active=True)
+            
+            username = os.environ['ADMIN_NAME']
+            email = os.environ['ADMIN_EMAIL']
+            password = os.environ['ADMIN_PASSWORD']
+            print('Creating account for %s (%s)' % (username, email))
+            admin = User.objects.create_superuser(
+                email=email, username=username, password=password, is_active=True)
+            gj = DistrictCollege.objects.create(district_name="Gujrat")
+            rj = DistrictCollege.objects.create(district_name="Rajasthan")
+            
+            ahm = CityCollegeModel.objects.create(district=gj, city_name="Ahmedabad")
+            rajkot = CityCollegeModel.objects.create(district=gj, city_name="Rajkot")
+            ajmer = CityCollegeModel.objects.create(district=rj, city_name="Ajmer")
+            jaipur = CityCollegeModel.objects.create(district=rj, city_name="Jaipur")
+            
+            ld = CollegeModel.objects.create(city=ahm, college_name="LD College of Engineering")
+            gec = CollegeModel.objects.create(city=rajkot, college_name="GEC")
+            
+            ajmed_ld = CollegeModel.objects.create(city=ajmer, college_name="LD College of Engineering, Ajmer branch")
+            jaipur_branch = CollegeModel.objects.create(city=jaipur, college_name="LD College of Engineering, Jaipur branch")
+            
+            
+            ec_branch = CollegeBranchModel.objects.create(college=ld, branch_name="Electronics and Communication")
+            computer_branch = CollegeBranchModel.objects.create(college=ld, branch_name="Comupter")
+            
+            admin_up = UserProfile.objects.get(user=admin)
+            admin_up.gender="M"
+            admin_up.college=ld
+            admin_up.city = ahm
+            admin_up.branch = ec_branch
+                
+                
+                
+                
+                
         else:
             print('Admin accounts can only be initialized if no Accounts exist')
